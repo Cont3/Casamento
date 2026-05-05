c34e1125a9fd8f7d0560ac94fb87c7c68e61de3c
Cont3 <36520929+Cont3@users.noreply.github.com>
2026-05-05 09:30:00 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,32 +1,29 @@
-document.querySelector(".envelope").onclick = () => {
-  document.getElementById("envelope-screen").style.display = "none";
-  document.getElementById("book").style.display = "block";
-
-  new Swiper('.swiper', {
-    direction: 'horizontal',
-  });
-};
 function abrirConvite() {
   const envelope = document.querySelector('.envelope');
   const tela = document.getElementById('envelope-screen');
   const book = document.getElementById('book');
 
-  // 1. abrir tampa
+  // abrir tampa
   envelope.classList.add('open');
 
-  // 2. subir carta
+  // subir + zoom
   setTimeout(() => {
     envelope.classList.add('zoom');
   }, 900);
 
-  // 3. transição para o conteúdo
+  // fade
   setTimeout(() => {
     tela.classList.add('fade-out');
   }, 1600);
 
-  // 4. mostrar livro
+  // mostrar livro + iniciar swiper
   setTimeout(() => {
     tela.style.display = 'none';
     book.style.display = 'block';
+
+    new Swiper('.swiper', {
+      direction: 'horizontal',
+    });
+
   }, 2300);
 }
