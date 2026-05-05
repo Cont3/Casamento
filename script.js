cf09605c3da6be1ae101f9cd45ded9b28428ce06
Cont3 <36520929+Cont3@users.noreply.github.com>
2026-05-05 09:26:30 -0300

    Update script.js

--- script.js
+++ script.js
@@ -6,3 +6,27 @@ document.querySelector(".envelope").onclick = () => {
     direction: 'horizontal',
   });
 };
+function abrirConvite() {
+  const envelope = document.querySelector('.envelope');
+  const tela = document.getElementById('envelope-screen');
+  const book = document.getElementById('book');
+
+  // 1. abrir tampa
+  envelope.classList.add('open');
+
+  // 2. subir carta
+  setTimeout(() => {
+    envelope.classList.add('zoom');
+  }, 900);
+
+  // 3. transição para o conteúdo
+  setTimeout(() => {
+    tela.classList.add('fade-out');
+  }, 1600);
+
+  // 4. mostrar livro
+  setTimeout(() => {
+    tela.style.display = 'none';
+    book.style.display = 'block';
+  }, 2300);
+}
